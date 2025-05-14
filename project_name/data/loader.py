226bfe0ff92c6dd28a82349724de2ef485fecfe0
ebekort <e.kort.3@student.rug.nl>
2025-05-14 16:16:08 +0200

    last commit did not add the loader file, so pushing that again

---
+++ project_name/data/loader.py
@@ -0,0 +1,18 @@
+
+import kagglehub
+import os
+import shutil
+
+path = kagglehub.dataset_download("orvile/brain-cancer-mri-dataset")
+destination_folder = "./data/"
+
+shutil.copy(os.path.join(path, 'dataset.csv'), os.path.join(destination_folder, 'dataset.csv'))
+
+for filename in os.listdir(os.path.join(path, 'Brain_Cancer raw MRI data/Brain_Cancer')):
+    os.mkdir(os.path.join(destination_folder, filename))
+    for image in os.listdir(os.path.join(path, 'Brain_Cancer raw MRI data/Brain_Cancer', filename)):
+        shutil.copy(os.path.join(path, 'Brain_Cancer raw MRI data/Brain_Cancer', filename, image), os.path.join(destination_folder, filename, image))
+    
+
+
+print(os.listdir(path))
