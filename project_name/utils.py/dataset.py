9b11f7687769e771d87eb9df4a9552b2ebd7e438
ebekort <e.kort.3@student.rug.nl>
2025-05-14 16:29:10 +0200

    adding dataset class that loads the data and adds function to transform pictures into numpy arrays

---
+++ project_name/utils.py/dataset.py
@@ -0,0 +1,39 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+import torch
+import numpy as np
+
+class MRI_Dataset(Dataset):
+  def __init__(self, data_dir, classes, transform):
+    self.data_dir = data_dir
+    self.classes = classes
+    self.class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
+    self.transform = transform
+
+    self.dataset = self.load_dataset(self.data_dir)
+
+
+  def load_dataset(self, data_dir):
+    data = []
+    for cls in self.classes:
+      cls_dir = os.path.join(data_dir, cls)
+      for fname in os.listdir(cls_dir):
+        fpath = os.path.join(cls_dir, fname)
+        data.append((fpath, self.class_to_idx[cls]))
+    return data
+
+
+  def __len__(self):
+    return len(self.dataset)
+
+
+  def __getitem__(self, idx):
+    img_path, label = self.dataset[idx]
+    img = Image.open(img_path).convert("RGB")
+    img = np.array(img) / 255.0
+    img = np.transpose(img, (2, 0, 1))
+    # Convert to one channel maybe since it is just black/white data?? but that might conflict with resnet, and normalize right away?
+    onehot = np.zeros(len(self.classes))
+    onehot[label] = 1 # maybe we need dtype=long instead?
+    return img, onehot
