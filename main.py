40d9826e280b79ac63f0a1465c73cf0cc9497df3
ebekort <e.kort.3@student.rug.nl>
2025-05-14 17:33:34 +0200

    adding first try of implementing a model in our pipeline

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from project_name.utils.loader import get_dataloaders
 import os
-
+from project_name.utils.loader import get_dataloaders
+from project_name.models.main_model import Model
 
 def main():
     classes = [cls for cls in os.listdir('./data') if not cls.endswith('.csv')]
     print(f'classes: {classes}')
     train_loader, val_loader, test_loader = get_dataloaders('./data', classes)
+    model = Model()
+    for images, labels in train_loader:
+        model.forward(images)
 
 if __name__ == '__main__':
     main()
